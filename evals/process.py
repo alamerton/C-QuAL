ef6c4869f3ce7044efc8695c2de71a60f8388009
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2024-10-14 11:06:53 +0100

    Removed n matching pairs and spurious correlations processing step

--- evals/process.py
+++ evals/process.py
@@ -5,37 +5,6 @@ DATASET_PATH = "data/processing/annotated_high_quality_pairs.csv"
 SAVE_PATH = "data/processing/annotated_high_quality_pairs.csv"
 
 
-# Open a dataset and remove any row where the answer contains an
-# exact match of a given length
-def remove_n_matching_pairs(question, answer, n):
-    question = str(question) if pd.notna(question) else ""
-    answer = str(answer) if pd.notna(answer) else ""
-
-    question_words = re.findall(r"\b\w+\b", question.lower())
-    answer_words = re.findall(r"\b\w+\b", answer.lower())
-
-    for i in range(len(question_words) - 2):
-        for j in range(len(answer_words) - 2):
-            if question_words[i : i + n] == answer_words[j : j + n]:
-                return True
-    return False
-
-
-def remove_spurious_pairs(dataset_path, save_path):
-    df = pd.read_csv(dataset_path)
-    df_filtered = df[
-        ~df.apply(
-            lambda row: remove_n_matching_pairs(
-                row["Question"], row["Expected Answer"]
-            ),
-            3,
-            axis=1,
-        )
-    ]
-    df_filtered.to_csv(save_path)
-    print("New shape: ", df_filtered.shape)
-
-
 def remove_low_quality_pairs(dataset_path, save_path):
     df = pd.read_csv(dataset_path)
     no_zeros_df = df[~df["Annotation"].astype(str).str.contains("0")]
@@ -71,7 +40,7 @@ def combine_csv_files(csv_1_path, csv_2_path):
 
 def remove_missing_value_rows(dataset_path, save_path):
     df = pd.read_csv(dataset_path)
-    # df = df.drop('Discharge Summaries', axis=1)
+    # df = df.drop('Discharge Summaries', axi=1)
     df = df.dropna()
     df.to_csv(save_path)
 
