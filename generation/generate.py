84c1ededb06c88ed49ebcca0bcf6a3f713700ca5
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2024-12-04 12:33:51 +0000

    Debugging; including renaming the columns and replacing question type (which has been omitted for now) with the capability type, i.e., reasoning or planning

--- generation/generate.py
+++ generation/generate.py
@@ -12,7 +12,7 @@ from utils.misc import select_capability_type
 from utils.generation.check_quality_with_gpt import check_quality_with_gpt
 
 # Dataset size
-NUMBER_OF_QA_PAIRS: int = 1500
+NUMBER_OF_QA_PAIRS: int = 10
 
 # Control the ratio of reasoning and planning questions in the dataset
 # by setting the proportion of reasoning questions. They can be any
@@ -25,10 +25,10 @@ PLANNING_Q_PROPORTION: int = 50
 CHECKPOINT: int = 0
 
 # Model for generating QA pairs
-QA_GENERATION_MODEL = "gpt-4o"
+QA_GENERATION_MODEL = "gpt-35-turbo-16k"
 
 # Model for quality-checking QA pairs
-QUALITY_CHECKING_MODEL = "gpt-4o"
+QUALITY_CHECKING_MODEL = QA_GENERATION_MODEL
 
 # Variable for limiting the number of consecutive summaries added to the
 # prompt (when multiple consecutive summaries belong to same patient).
@@ -39,17 +39,12 @@ MAX_SUMMARIES: int = 3
 
 def main():
     # create dataframe with question and expected answer columns
-    if MAX_SUMMARIES > 1:
-        discharge_summary_string = "Discharge Summaries"
-    else:
-        discharge_summary_string = "Discharge Summary"
-
     data = pd.DataFrame(
         columns=[
-            discharge_summary_string,
+            "Evidence",
             "Question",
             "Expected Answer",
-            "Question Type",
+            "Capability",
         ]
     )
 
@@ -66,7 +61,8 @@ def main():
         date = date.strftime("%Y-%m-%d %H:%M:%S")
 
         # Create data item starting with discharge summary
-        data_item = [discharge_summaries[row]]
+        data_item = []
+        discharge_summary = discharge_summaries[row]
 
         capability_type = select_capability_type(
             REASONING_Q_PROPORTION, PLANNING_Q_PROPORTION
@@ -76,19 +72,41 @@ def main():
         while "1" not in quality_checking_result:
 
             # Call LLM with discharge summary and prompt
-            qa_string = call_gpt(QA_GENERATION_MODEL, data_item, capability_type)
+            qa_string = call_gpt(
+                QA_GENERATION_MODEL, discharge_summary, capability_type
+            )
+
+            print("QA String: ", qa_string)
 
             # Check correct columns are in response, regenerate until true
-            while (
-                "Question" not in qa_string
-                or "Answer" not in qa_string
-                or "Type" not in qa_string
-            ):
-                qa_string = call_gpt(QA_GENERATION_MODEL, data_item)
+            if capability_type == "planning":
+                while "Question" not in qa_string or "Answer" not in qa_string:
+                    qa_string = call_gpt(
+                        QA_GENERATION_MODEL, discharge_summary, capability_type
+                    )
+            else:
+                while (
+                    "Question" not in qa_string
+                    or "Answer" not in qa_string
+                    or "Evidence" not in qa_string
+                ):
+                    qa_string = call_gpt(
+                        QA_GENERATION_MODEL, data_item, capability_type
+                    )
 
             quality_checking_result = check_quality_with_gpt(
                 qa_string, QUALITY_CHECKING_MODEL, capability_type
             )
+            # If the quality checking function returns a string that is
+            # not either '0' or '1', retry until it is
+            while (
+                "1" not in quality_checking_result
+                and "0" not in quality_checking_result
+            ):
+                quality_checking_result = check_quality_with_gpt(
+                    qa_string, QUALITY_CHECKING_MODEL, capability_type
+                )
+                print("Quality checking result: ", quality_checking_result)
 
         # Parse the json to get the question and answer as variables
         qa_parts = qa_string.split("\n")
@@ -97,11 +115,12 @@ def main():
         print(qa_parts)  # Log the data to terminal
         question = qa_parts[0][10:]  # Remove "Question: "
         answer = qa_parts[1][8:]  # Remove "Answer: "
-        question_type = qa_parts[2][6:]  # Remove "Type: "
-
-        # Add data to data item
-        data_item.extend((question, answer, question_type))
-
+        if capability_type == "reasoning":
+            evidence = qa_parts[2][10:]  # Remove "Evidence: "
+            # Add data to data item
+            data_item.extend((evidence, question, answer, capability_type))
+        else:
+            data_item.extend(("", question, answer, capability_type))
         # Add Q-A pair to dataframe
         data.loc[row] = data_item
 
